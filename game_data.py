64fc519fc91b020ea12e86a18245ad9dd2084b0d
ngiuliano96 <118212081+ngiuliano96@users.noreply.github.com>
2023-06-05 04:56:22 +0000

    Initial commit

---
+++ game_data.py
@@ -0,0 +1,302 @@
+data = [
+    {
+        'name': 'Twitter',
+        'follower_count': 65.7,
+        'description': 'Social media platform',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 108.6,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Elon Musk',
+        'follower_count': 140.8,
+        'description': 'CEO and engineer',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Dwayne Johnson',
+        'follower_count': 17,
+        'description': 'Actor and professional wrestler',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Selena Gomez',
+        'follower_count': 67,
+        'description': 'Musician and actress',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Kylie Jenner',
+        'follower_count': 40.2,
+        'description': 'Reality TV personality and businesswoman and Self-Made Billionaire',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'follower_count': 75.1,
+        'description': 'Reality TV personality and businesswoman',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Tom Brady',
+        'follower_count': 3,
+        'description': 'American football player',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Beyoncé',
+        'follower_count': 15.5,
+        'description': 'Musician',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Neymar',
+        'follower_count': 62.1,
+        'description': 'Footballer',
+        'country': 'Brasil'
+    },
+    {
+        'name': 'National Geographic',
+        'follower_count': 28.8,
+        'description': 'Magazine',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Justin Bieber',
+        'follower_count': 112.6,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Taylor Swift',
+        'follower_count': 92.8,
+        'description': 'Musician',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Bill Nye',
+        'follower_count': 5.8,
+        'description': 'TV personality and mechanical engineer',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Jennifer Lopez',
+        'follower_count': 45.1,
+        'description': 'Musician and actress',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Nicki Minaj',
+        'follower_count': 27.9,
+        'description': 'Musician',
+        'country': 'Trinidad and Tobago'
+    },
+    {
+        'name': 'Nike',
+        'follower_count': 9.7,
+        'description': 'Sportswear multinational',
+        'country': 'The United States'
+    },
+    {
+        'name': 'J.K. Rowling',
+        'follower_count': 14,
+        'description': 'Author',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Joji (George Miller)',
+        'follower_count': 1.7,
+        'description': 'Musician and retired Youtuber',
+        'country': 'Japan'
+    },
+    {
+        'name': 'Katy Perry',
+        'follower_count': 107.9,
+        'description': 'Musician',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Stephen King',
+        'follower_count': 7.1,
+        'description': 'Author',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Kevin Hart',
+        'follower_count': 37.2,
+        'description': 'Comedian and actor',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Dr. Phil',
+        'follower_count': 1.4,
+        'description': 'TV personality and author',
+        'country': 'The United States'
+    },
+    {
+        'name': 'NFL',
+        'follower_count': 33,
+        'description': 'American football club',
+        'country': 'The United States'
+    },
+    {
+        'name': 'FC Barcelona',
+        'follower_count': 47.2,
+        'description': 'Football club',
+        'country': 'Spain'
+    },
+    {
+        'name': 'Rihanna',
+        'follower_count': 108.3,
+        'description': 'Musician and businesswoman',
+        'country': 'Barbados'
+    },
+    {
+        'name': 'Pink Floyd',
+        'follower_count': 3.4,
+        'description': 'Rock band',
+        'country': 'England'
+    },
+    {
+        'name': 'Forever 21',
+        'follower_count': 1.8,
+        'description': 'Clothing brand',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Zendaya',
+        'follower_count': 20.9,
+        'description': 'Actress and musician',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Shakira',
+        'follower_count': 53.9,
+        'description': 'Musician',
+        'country': 'Colombia'
+    },
+    {
+        'name': 'Drake',
+        'follower_count': 39.6,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Mike Trout',
+        'follower_count': 2.5,
+        'description': 'Baseball player',
+        'country': 'The United States'
+    },
+    {
+        'name': 'LeBron James',
+        'follower_count': 52.8,
+        'description': 'Basketball player',
+        'country': 'The United States'
+    },
+    {
+        'name': 'Tom Hiddleston',
+        'follower_count': 4.1,
+        'description': 'Actor',
+        'country': 'English'
+    },
+    {
+        'name': 'Cardi B',
+        'follower_count': 30,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'UFC',
+        'follower_count': 11.4,
+        'description': 'Mixed martial arts promotion company',
+        'country': 'United States'
+    },
+    {
+        'name': 'Billie Eilish',
+        'follower_count': 7.2,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Justin Timberlake',
+        'follower_count': 61.9,
+        'description': 'Musician and actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'FIFA',
+        'follower_count': 16.3,
+        'description': 'International football association',
+        'country': 'Europe'
+    },
+    {
+        'name': 'NASA',
+        'follower_count': 56,
+        'description': 'Space agency',
+        'country': 'United States'
+    },
+    {
+        'name': 'Emma Watson',
+        'follower_count': 27.8,
+        'description': 'Actress',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Shawn Mendes',
+        'follower_count': 26.3,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Virat Kohli',
+        'follower_count': 55.9,
+        'description': 'Cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'Markiplier',
+        'follower_count': 13.9,
+        'description': 'Youtuber',
+        'country': 'United States'
+    },
+    {
+        'name': 'Priyanka Chopra Jonas',
+        'follower_count': 27.8,
+        'description': 'Actress and musician',
+        'country': 'India'
+    },
+    {
+        'name': '9GAG',
+        'follower_count': 16.8,
+        'description': 'Social media platform',
+        'country': 'China'
+    },
+    {
+        'name': 'Ronaldinho',
+        'follower_count': 21.5,
+        'description': 'Footballer',
+        'country': 'Brasil'
+    },
+    {
+        'name': 'Maluma',
+        'follower_count': 8.4,
+        'description': 'Musician',
+        'country': 'Colombia'
+    },
+    {
+        'name': 'Camila Cabello',
+        'follower_count': 13,
+        'description': 'Musician',
+        'country': 'Cuba'
+    },
+    {
+        'name': 'NBA',
+        'follower_count': 43.4,
+        'description': 'Club Basketball Competition',
+        'country': 'United States'
+    }
+]
---
+++ main.py
@@ -0,0 +1,48 @@
+import random
+from replit import clear
+import art
+from game_data import data
+
+def compare_followers(a, b, guess):
+    followers_a = a["follower_count"]
+    followers_b = b["follower_count"]
+
+    if followers_a > followers_b:
+        return guess == "a"
+    else:
+        return guess == "b"
+
+def display_message(person):
+    return f"{person['name']}, the {person['description']} from {person['country']}"
+
+# Set initial person A
+person_a = random.choice(data)
+score = 0
+
+print(art.logo)
+
+is_playing = True
+while is_playing:
+    # Set person B
+    person_b = random.choice(data)
+    
+    print(f"Compare A: {display_message(person_a)}")
+    print(art.vs)
+    print(f"Against B: {display_message(person_b)}")
+    
+    # Collect player's guess
+    player_guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    
+    if compare_followers(person_a, person_b, player_guess):
+        score += 1
+        # Set next person A to previous person B
+        person_a = person_b
+       
+        clear()
+        print(art.logo)
+        print(f"You're right! Current score: {score}")
+    else:
+        clear()
+        print(art.logo)
+        print(f"Sorry, that's wrong. Final score: {score}")
+        is_playing = False
